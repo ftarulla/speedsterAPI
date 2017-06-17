66508f89ac1692f4ba8e0c87cb26ffeae40215b4
Fran <ftarulla@gmail.com>
2017-06-17 18:14:12 -0300

    Nuevo help con la API completa.

--- help.js
+++ help.js
@@ -11,7 +11,33 @@ var help = {
             '/api/tracks/:track_id': {
                 "descripción": "Obtiene el circuito con id :track_id",
                 "ejemplo": "/api/tracks/42"
-            }
+            },
+            '/api/webcams/:track_id': {
+                "descripción": "Lista las cámaras del circuito con id :track_id.",
+                "ejemplo": "/api/webcams/42"
+            },
+            '/api/webcams/:track_id/:webcam_id': {
+                "descripción": "Obtiene la cámara con id :webcam_id del circuito con id :track_id",
+                "ejemplo": "/api/webcams/42/86"
+            },
+            // A partir de acá las urls dan a entender que el circuito está preseleccionado
+            // y no se pasa en la URL ... INPOT!!
+            '/api/runners/': {
+                "descripción": "Lista los corredores de la carrera.",
+                "ejemplo": "/api/runners/"
+            },
+            '/api/runners/:runner_id': {
+                "descripción": "Obtiene el corredor con id :runner_id",
+                "ejemplo": "/api/runners/780"
+            },
+            '/api/positions/': {
+                "descripción": "Lista las posiciones de todos los corredores.",
+                "ejemplo": "/api/positions/"
+            },
+            '/api/positions/:runner_id': {
+                "descripción": "Obtiene las posiciones del corredor con id :runner_id",
+                "ejemplo": "/api/positions/780"
+            },
         }
     }
 }
--- positions.js
+++ positions.js
@@ -1,11 +1,11 @@
 
 var runnerService = require("./runners.js");
 
-var positions = [];
-//var positions = {};
-
+//
 var runners = runnerService.list();
 
+//
+var positions = [];
 var position0 = {
     runner: runners[0].id,
     positions: [
@@ -18,13 +18,6 @@ var position0 = {
 }
 positions.push(position0);
 
-// positions[runners[0].id] = [
-//     {lat: -34.522739, lon: -58.702329},
-//     {lat: -34.522992, lon: -58.702572},
-//     {lat: -34.523191, lon: -58.702801},
-//     {lat: -34.523412, lon: -58.703056},
-//     {lat: -34.523643, lon: -58.703299}
-// ];
 
 
 /******************************************************************************
